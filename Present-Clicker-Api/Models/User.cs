9708ac82a375bf0aab3fb57fc86633f75232a32f
HarrisonJ05 <harrisonjeffreys101@gmail.com>
2024-12-12 10:51:52 +0000

    Created the backend api folder and began creating the models and db

---
+++ Present-Clicker-Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Present_Clicker_Api.Models
+{
+    internal class User
+    {
+    }
+}
